38abfc5212bd84d4cea152e5bf6fe3ec814d1381
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-02-03 14:46:48 +0530

    base version

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShanOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Blockchain World!");
+        }
+    }
+}
