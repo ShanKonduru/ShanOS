f690390df989a703de61b150e3f7f103b950bcd8
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-02-03 15:13:16 +0530

    added  Main program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace ShanOS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Blockchain World!");
+            UInt64 index=0;
+            Block genesisBlock = new Block(index++, "Genesis Block");
+            Console.WriteLine(genesisBlock.ToPrint());
+            Block b1 = new Block(index++, "First Block", genesisBlock.CurrentHash );
+            Console.WriteLine(b1.ToPrint());
+            Block b2 = new Block(index++, "First Block", b1.CurrentHash );
+            Console.WriteLine(b2.ToPrint());
+            b1.Data="Made a Change";
+            Console.WriteLine(b2.ToPrint());
         }
     }
 }
